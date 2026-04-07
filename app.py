96572d14fa3c3c74e55385168361b99c1606c4dd
khm75398214-png <khm75398214@gmail.com>
2026-04-07 15:55:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "봇 서버 정상 작동중!"
+@app.route('/', methods=['POST'])
+def kakao():
+    data = request.get_json()
+    
+    user_msg = data['userRequest']['utterance']
+
+    response = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": f"너가 보낸 말: {user_msg}"
+                    }
+                }
+            ]
+        }
+    }
+
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
