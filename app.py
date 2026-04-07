07df4f5725d8d73ad325cd0d71ed7888f2c3aa5c
khm75398214-png <khm75398214@gmail.com>
2026-04-08 08:40:23 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
