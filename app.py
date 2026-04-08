9d4c174a19f89355207a448accc802a58558d4d2
khm75398214-png <khm75398214@gmail.com>
2026-04-08 14:18:40 +0900

    app.py 업데이트

--- app.py
+++ app.py
@@ -1,46 +1,31 @@
-from flask import Flask, request
-import firebase_admin
-from firebase_admin import credentials, firestore
+user_msg = req["userRequest"]["utterance"]
 
-app = Flask(__name__)
-
-# Firebase 연결
-cred = credentials.Certificate("firebase.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
-
-@app.route("/bot", methods=["POST"])
-def bot():
-    data = request.get_json(force=True)  # 🔥 이걸로 수정
-
-    if not data:
-        return "No data"
-
-    user_msg = data["userRequest"]["utterance"]
-
-    # 명령어 처리
-    if user_msg == "핑":
-        return {
-            "version": "2.0",
-            "template": {
-                "outputs": [{"simpleText": {"text": "퐁"}}]
-            }
-        }
-
-    elif user_msg.startswith("!경고"):
-        return {
-            "version": "2.0",
-            "template": {
-                "outputs": [{"simpleText": {"text": "경고 1회"}}]
-            }
-        }
-
-    # 기본 응답
+if user_msg == "핑":
     return {
         "version": "2.0",
         "template": {
-            "outputs": [{"simpleText": {"text": "몰루"}}]
+            "outputs": [
+                {"simpleText": {"text": "퐁"}}
+            ]
         }
     }
 
-app.run(host="0.0.0.0", port=5000)
+elif user_msg == "경고":
+    return {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {"simpleText": {"text": "경고 1회"}}
+            ]
+        }
+    }
+
+else:
+    return {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {"simpleText": {"text": "몰루"}}
+            ]
+        }
+    }
